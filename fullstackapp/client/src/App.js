57dbab488a8695a3d51d48729d481a95ab829961
MadisonBoone109 <87313571+W046871822@users.noreply.github.com>
2025-02-09 23:08:12 -0400

    add image and sticker examples

--- fullstackapp/client/src/App.js
+++ fullstackapp/client/src/App.js
@@ -20,10 +20,24 @@ export default function App() {
   ];
 
   const stickers = [
-    { id: "sticker1", image: "https://via.placeholder.com/50" },
-    { id: "sticker2", image: "https://via.placeholder.com/50" }
+    { id: "sticker1", image: "https://i.pinimg.com/236x/43/33/6e/43336ef5c93653fc193a71b8de41b3cf.jpg" },
+    { id: "sticker2", image: "https://i.pinimg.com/474x/6d/34/1a/6d341a0c3f324cffa041250306647554.jpg" },
+    { id: "sticker3", image: "https://i.pinimg.com/736x/7a/d0/ce/7ad0ceedf8324711618221370042e6bb.jpg" },
+    { id: "sticker4", image: "https://i.pinimg.com/236x/b0/da/94/b0da94af3eff110610dae2b7318659a6.jpg" },
+    { id: "sticker5", image: "https://i.pinimg.com/236x/5e/f6/44/5ef644bae7fdd0df00d485796a70ad18.jpg" },
+    { id: "sticker6", image: "https://i.pinimg.com/236x/3c/c4/4a/3cc44a15a9f1f6aeaf970f5588e36f5a.jpg" },
+    { id: "sticker7", image: "https://i.pinimg.com/736x/59/8b/c7/598bc768b164edaac221470a3aaa0eb1.jpg" }
   ];
 
+  const images = [
+    { id: "image1", image: "https://i.pinimg.com/236x/38/44/0b/38440b7dcf5ba62424cc4a6cac3d75d5.jpg" },
+    { id: "image2", image: "https://i.pinimg.com/236x/0a/c0/d5/0ac0d5e388101b12a5f1db1da19c01fc.jpg" },
+    { id: "image3", image: "https://i.pinimg.com/236x/ba/68/20/ba68204ca9c4212c49d564116762678e.jpg" },
+    { id: "image4", image: "https://i.pinimg.com/236x/9c/43/da/9c43dacca48a9ece41cf32dffa416de3.jpg" },
+    { id: "image5", image: "https://i.pinimg.com/236x/39/83/49/398349937d7d4a667e629f5f9d1bcdb7.jpg" },
+    { id: "image6", image: "https://i.pinimg.com/236x/9f/20/a6/9f20a60d1c957f5d7b0e963e91401c2c.jpg" },
+    { id: "image7", image: "https://i.pinimg.com/236x/09/3e/cf/093ecf7e5e866d1d9b38a17159db7bd0.jpg" }
+  ];
 
   const handleDragStart = (e, item) => {
     e.dataTransfer.setData("item", JSON.stringify(item));
@@ -72,29 +86,24 @@ export default function App() {
           </div>
 
           <div className="items">
-            {tab === "songs" &&
-              songs.map((song) => (
-                <div key={song.id} className="draggable" draggable onDragStart={(e) => handleDragStart(e, song)}>
-                  <img src={song.album.cover} alt={song.title} />
-                  <p>{song.title}</p>
-                </div>
-              )
-              )}
-
-{songs && songs.length > 0 ? (
-  songs.map((song, index) => (
-    <div key={index}>
-      <img src={song.album?.cover || "default-cover.jpg"} alt={song.title} />
-      <p>{song.title}</p>
-    </div>
-  ))
-) : (
-  <p>No songs found</p>
-)}
-
+          {tab === "songs" &&
+            songs.map((song) => (
+              <div key={song.id} className="draggable" draggable onDragStart={(e) => handleDragStart(e, song)}>
+                <img src={song.album?.cover || "https://via.placeholder.com/80"} alt={song.title} />
+                <p>{song.title}</p>
+              </div>
+            ))
+          }
 
             {tab === "stickers" &&
               stickers.map((sticker) => (
+                <div key={sticker.id} className="draggable" draggable onDragStart={(e) => handleDragStart(e, sticker)}>
+                  <img src={sticker.image} alt="Sticker" />
+                </div>
+              ))}
+
+            {tab === "images" &&
+              images.map((sticker) => (
                 <div key={sticker.id} className="draggable" draggable onDragStart={(e) => handleDragStart(e, sticker)}>
                   <img src={sticker.image} alt="Sticker" />
                 </div>
@@ -104,14 +113,17 @@ export default function App() {
 
         {/* Centered Canvas */}
         <div className="canvas" onDragOver={(e) => e.preventDefault()} onDrop={handleDrop}>
-          {canvasItems.map((item, index) => (
-            <div key={index} className="grid-item" style={{ left: `${item.x * 100}px`, top: `${item.y * 100}px` }}>
-              <img src={item.album.cover} alt={item.title} />
-              <audio controls>
-                <source src={item.preview} type="audio/mp3" />
-              </audio>
-            </div>
-          ))}
+        {canvasItems.map((item, index) => (
+  <div key={index} className="grid-item" style={{ left: `${item.x * 100}px`, top: `${item.y * 100}px` }}>
+    <img src={item.album?.cover || item.image || "https://via.placeholder.com/80"} alt={item.title || "Sticker"} />
+    {item.preview && (
+      <audio controls>
+        <source src={item.preview} type="audio/mp3" />
+      </audio>
+    )}
+  </div>
+))}
+
         </div>
       </div>
 
